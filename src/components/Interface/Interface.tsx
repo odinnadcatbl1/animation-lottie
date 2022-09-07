ba18752d6d41e82a575796902d7c3b6dd50586e3
Salavat F <salmrproperfahr@mail.ru>
2022-09-07 18:59:14 +0300

    create interface component

---
+++ src/components/Interface/Interface.tsx
@@ -0,0 +1,9 @@
+const Interface: React.FC = () => {
+    return (
+        <div className="interface">
+            <div className="interface__inner">interface</div>
+        </div>
+    );
+};
+
+export default Interface;
