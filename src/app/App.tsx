b9366ed42dd890d72eee5d865d80018780b52cac
Salavat F <salmrproperfahr@mail.ru>
2022-09-07 18:59:57 +0300

    fixed app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,34 @@
 import Button from "../components/Button/Button";
+import Interface from "../components/Interface/Interface";
+import { useState } from "react";
+import { delay } from "../tools/delay";
 
 import "./app.css";
+import Loading from "../components/Loading/Loading";
 
 function App() {
+    const [buttonIsVisible, setButtonVisible] = useState(true);
+    const [animationIsVisible, setAnimationIsVisible] = useState(false);
+
     const onButtonClick = () => {
-        console.log(1);
+        setButtonVisible(false);
+        setAnimationIsVisible(true);
+
+        delay(3000).then(() => {
+            setAnimationIsVisible(false);
+            setButtonVisible(true);
+        });
     };
 
     return (
         <div className="app">
             <div className="container">
                 <div className="app__inner">
-                    <Button text={"Click me"} onClick={onButtonClick} />
+                    {buttonIsVisible && (
+                        <Button text={"Click me"} onClick={onButtonClick} />
+                    )}
+
+                    {animationIsVisible && <Loading />}
                 </div>
             </div>
         </div>
